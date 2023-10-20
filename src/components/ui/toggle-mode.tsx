f2e5115f7f9143613100ca4206fb286f06f24bb8
ImMiguelP <anarchxzx@gmail.com>
2023-10-20 16:04:04 -0700

    feat: dark mode

--- src/components/ui/toggle-mode.tsx
+++ src/components/ui/toggle-mode.tsx
@@ -1,40 +1,43 @@
-"use client";
-
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+
+  // Use state to store the color classes
+  const [sunColor, setSunColor] = React.useState(
+    theme === "light" ? "text-yellow-400" : "text-gray-500"
+  );
+  const [moonColor, setMoonColor] = React.useState(
+    theme === "dark" ? "text-cyan-800-500" : "text-gray-500"
+  );
+
+  // Use useEffect to update the icon colors when the theme changes
+  React.useEffect(() => {
+    if (theme === "light") {
+      setSunColor("text-yellow-400");
+      setMoonColor("text-gray-500");
+    } else if (theme === "dark") {
+      setSunColor("text-gray-500");
+      setMoonColor("text-cyan-800-500");
+    }
+  }, [theme]);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="flex space-x-2">
+      <Button variant="link" size="icon" onClick={() => setTheme("light")}>
+        <Sun
+          className={`h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all ${sunColor}`}
+        />
+      </Button>
+
+      <Button variant="link" size="icon" onClick={() => setTheme("dark")}>
+        <Moon
+          className={`h-[1.2rem] w-[1.2rem] rotate-90 scale-100 transition-all ${moonColor}`}
+        />
+      </Button>
+    </div>
   );
 }
