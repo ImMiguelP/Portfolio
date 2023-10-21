313d35b108dfd9df2198c18f828b523028f96ad7
ImMiguelP <anarchxzx@gmail.com>
2023-10-21 14:52:55 -0700

    style: header done

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+"use client";
+import React, { use } from "react";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section className="py-24 flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold"> Hi, Im Miguel </h1>
+      <h1 className="text-4xl font-bold text-center">
+        Hello, I&apos;m <br></br>
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-900 to-blue-400 ">
+          <TypeAnimation
+            sequence={[
+              "Miguel",
+              1000,
+              "A Web Developer",
+              1000,
+              "A Software Engineer",
+              1000,
+              "A Ui/Ux Designer",
+              1000,
+            ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+          />
+        </span>
+      </h1>
     </section>
   );
 };
