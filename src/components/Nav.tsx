4cb59b8b23f2a3e1a777c7b7e7b3a8dd28979913
ImMiguelP <anarchxzx@gmail.com>
2023-10-22 16:09:51 -0700

    style: add styling to nav bar when hovering icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -18,20 +18,34 @@ const Nav = () => {
   ];
 
   return (
-    <div className="flex items-center justify-center ">
+    <div className="flex items-center justify-center py-7 space-x-3 ">
       {buttons.map(({ path, icon }) => (
         <Link key={path} href={path}>
           <Button
             variant="link"
-            className={`text-xl ${
+            className={`text-xl px-2 ${
               pathname === path ? "text-primary" : " text-gray-600"
             }`}
           >
-            {icon}
+            <span
+              className={`group relative inline-flex items-center py-2 ${
+                pathname === path
+                  ? "hover:text-blue-500"
+                  : "group-hover:text-blue-500"
+              }`}
+            >
+              {icon}
+              <span
+                className={`absolute w-full h-0.5 bottom-0 left-0 bg-blue-500 transition-transform duration-300 scale-x-0 group-hover:scale-x-100`}
+              ></span>
+            </span>
           </Button>
         </Link>
       ))}
-      <Separator orientation="vertical" />
+      <Separator
+        orientation="vertical"
+        className="h-3 min-h-full bg-primary mx-1"
+      />
       <ModeToggle />
     </div>
   );
