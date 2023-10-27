efcdc627a17a24aecfe472e9f27292dc0b45fe53
ImMiguelP <anarchxzx@gmail.com>
2023-10-26 18:26:43 -0700

    style: padding adjust for responsive site

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Technologies from "@/components/Technologies/Technologies";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center p-4 lg:p-24">
       <HeroSection />
       <header className="text-2xl font-bold text-center pb-5">
         Tech I Use
--- app/troll/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const troll = () => {
-  return <div>troll</div>;
-};
-
-export default troll;
