4fc502ce1d3e4d10fd7c65b738dafb94f7557095
ImMiguelP <anarchxzx@gmail.com>
2023-11-01 14:25:37 -0700

    style: add hue to nav with separator

--- src/components/NavBar/Nav.tsx
+++ src/components/NavBar/Nav.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import BrowserNav from "./BrowserNav";
 import MobileNav from "./MobileNav";
+import { Separator } from "../ui/separator";
 
 const Nav = () => {
   return (
     <nav>
       <BrowserNav />
       <MobileNav />
+      <Separator className="bg-green-300 fixed" />
     </nav>
   );
 };
