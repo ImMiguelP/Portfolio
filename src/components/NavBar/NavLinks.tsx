ba200b2833b6ac68c0584a9bb6fdfad1b83252f0
ImMiguelP <anarchxzx@gmail.com>
2023-12-17 16:31:15 -0800

    chore: add blog

--- src/components/NavBar/NavLinks.tsx
+++ src/components/NavBar/NavLinks.tsx
@@ -1,7 +1,9 @@
 import { RiHomeHeartFill } from "react-icons/ri";
 import { SiMinutemailer } from "react-icons/si";
+import { IoLogoWordpress } from "react-icons/io5";
 
 export const navlinks = [
-    { path: "/", icon: <RiHomeHeartFill />, name: "Home"},
-    { path: "/contact-me", icon: <SiMinutemailer />, name: "Contact Me"},
-  ];
+  { path: "/", icon: <RiHomeHeartFill />, name: "Home" },
+  { path: "/contact-me", icon: <SiMinutemailer />, name: "Contact Me" },
+  { path: "/blog", icon: <IoLogoWordpress />, name: "My Blog" },
+];
