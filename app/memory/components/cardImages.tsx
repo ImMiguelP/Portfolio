8f90fd4aeb3360e98f5e1f675d1051c9e6214412
ImMiguelP <anarchxzx@gmail.com>
2024-02-03 16:39:38 -0800

    chore: fiel for images

---
+++ app/memory/components/cardImages.tsx
@@ -0,0 +1,19 @@
+import gobCelestial from "../components/images/gobCelestial.png";
+import gobFish from "../components/images/gobFish.png";
+import gobHero from "../components/images/gobHero.png";
+import gobMonkey from "../components/images/gobMonkey.png";
+import gobNinja from "../components/images/gobNinja.png";
+import gobSpace from "../components/images/gobSpace.png";
+import gobTree from "../components/images/gobTree.png";
+import gobWizard from "../components/images/gobWizard.png";
+
+export const cardImages = [
+  { src: gobCelestial, alt: "gobCelistial", matched: false },
+  { src: gobFish, alt: "gobFish", matched: false },
+  { src: gobHero, alt: "gobHero", matched: false },
+  { src: gobMonkey, alt: "gobMonkey ", matched: false },
+  { src: gobNinja, alt: "gobNinja", matched: false },
+  { src: gobSpace, alt: "gobSpace", matched: false },
+  { src: gobTree, alt: "gobTree", matched: false },
+  { src: gobWizard, alt: "gobWizard ", matched: false },
+];
