39568e727f6395fd4a965522b740bde98aebd12f
ImMiguelP <anarchxzx@gmail.com>
2024-04-04 11:08:33 -0700

    feat: timeline showcasing my work

---
+++ app/contact-me/components/TimeLineObj.tsx
@@ -0,0 +1,40 @@
+import { BriefcaseIcon, ComputerDesktopIcon } from "@heroicons/react/20/solid";
+
+export const timeline = [
+  {
+    id: 1,
+    company: "Instagram Clone",
+    target: "Creator",
+    href: "https://github.com/ImMiguelP/instagram-clone",
+    date: "2020",
+    icon: ComputerDesktopIcon,
+    iconBackground: "bg-green-500",
+  },
+  {
+    id: 2,
+    company: "Musy",
+    target: "Contributor",
+    href: "https://musy.one/home",
+    date: "2020-Present",
+    icon: ComputerDesktopIcon,
+    iconBackground: "bg-blue-500",
+  },
+  {
+    id: 3,
+    company: "InReach",
+    target: "Front End Developer",
+    href: "https://www.inreach.gg/",
+    date: "Nov 2021-2022",
+    icon: BriefcaseIcon,
+    iconBackground: "bg-green-500",
+  },
+  {
+    id: 4,
+    company: "Ethereum Website",
+    target: "Contributor",
+    href: "https://ethereum.org/en/",
+    date: "2022-Present",
+    icon: ComputerDesktopIcon,
+    iconBackground: "bg-blue-500",
+  },
+];
